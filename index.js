2cd66b24e2069860d8f9aaafabb9ca0874f12bb6
Barbara Stadnik <barbara1stadnik@gmail.com>
2020-03-28 17:31:42 +0100

    correct my solution

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const timeNodes = Array.from(document.querySelectorAll('[data-time]'));
+const videos = [...document.querySelectorAll('.videos li')];
 
-const seconds = timeNodes
-	.map(node => node.dataset.time)
-	.map(timeCode => {
-		const [mins, secs] = timeCode.split(':').map(parseFloat);
-		return (mins * 60) + secs;
-	})
-	.reduce((total, seconds) => total + seconds);
+const totalSeconds = videos.map(video => { 
+	const [mins, secs] = video.dataset.time.split(':').map(parseFloat);
+	return mins * 60 + secs;
+})
+.reduce((total, timeInSecs) => total + timeInSecs);
 
-let secondsLeft = seconds;
-const hours = Math.floor(secondsLeft / 3600);
-secondsLeft = secondsLeft % 3600;
+function getFinalTime() {
+	const hours = Math.floor(totalSeconds / 3600);
+	let secondsLeft = totalSeconds % 3600;
+	const minutes = Math.floor(secondsLeft / 60);
+	secondsLeft = secondsLeft % 60;
 
-const mins = Math.floor(secondsLeft / 60);
-secondsLeft = secondsLeft % 60;
+	return `${hours}:${minutes}:${secondsLeft}`;
+}
 
-console.log(hours, mins, secondsLeft);
+getFinalTime();
